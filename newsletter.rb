5047a89645834ef0de6a182751adcbabea97904b
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-14 10:59:59 -0800

    Done.

--- newsletter.rb
+++ newsletter.rb
@@ -32,8 +32,8 @@ def calculate_recipients
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+   binding.pry
 end
 
 def print_recipients
@@ -77,11 +77,12 @@ def print_newsletter(number)
   puts "\nBODY:"
   format_subject
   articles = first_n_articles(number)
+ 
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
 end
+
 
 def run
   # We want our program to print three articles by default,
